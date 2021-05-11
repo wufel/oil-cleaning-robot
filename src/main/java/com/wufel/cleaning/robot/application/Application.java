9192b252cbd7b7879d11b0d020b95363dac4e509
wufel <wufelixhk@gmail.com>
2021-05-11 10:25:01 +0100

    spring initializr setup

---
+++ src/main/java/com/wufel/cleaning/robot/application/Application.java
@@ -0,0 +1,13 @@
+package com.wufel.cleaning.robot.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
